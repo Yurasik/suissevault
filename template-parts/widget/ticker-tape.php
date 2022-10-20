45ab06e503ff9c6aac6e65afbc95a9a1494cb88c
Андрій К <kvandrij@gmail.com>
2022-10-20 12:45:53 +0000

    TradingView Widget

--- template-parts/widget/ticker-tape.php
+++ template-parts/widget/ticker-tape.php
@@ -1,36 +1,50 @@
-<!-- TradingView Widget BEGIN -->
-<div class="ranning tradingview-widget-container">
-	<div class="tradingview-widget-container__widget"></div>
-	<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-        {
-            "symbols": [
-            {
-                "proName": "FOREXCOM:SPXUSD",
-                "title": "S&P 500"
-            },
-            {
-                "proName": "FOREXCOM:NSXUSD",
-                "title": "US 100"
-            },
-            {
-                "proName": "FX_IDC:EURUSD",
-                "title": "EUR/USD"
-            },
-            {
-                "proName": "BITSTAMP:BTCUSD",
-                "title": "Bitcoin"
-            },
-            {
-                "proName": "BITSTAMP:ETHUSD",
-                "title": "Ethereum"
-            }
-        ],
-            "showSymbolLogo": true,
-            "colorTheme": "light",
-            "isTransparent": true,
-            "displayMode": "adaptive",
-            "locale": "en"
-        }
-	</script>
+<!-- TradingView Widget BEGIN 20/10/22-->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/" rel="noopener" target="_blank"><span class="blue-text">Markets</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+  {
+  "symbols": [
+    {
+      "description": "GBP/USD",
+      "proName": "OANDA:GBPUSD"
+    },
+    {
+      "description": "GBP/EUR",
+      "proName": "FX_IDC:GBPEUR"
+    },
+    {
+      "description": "XAU/GBP",
+      "proName": "OANDA:XAUGBP"
+    },
+    {
+      "description": "XAU/ USD",
+      "proName": "OANDA:XAUUSD"
+    },
+    {
+      "description": "XAU/ EUR",
+      "proName": "OANDA:XAUEUR"
+    },
+    {
+      "description": "XAG/ GBP",
+      "proName": "OANDA:XAGGBP"
+    },
+    {
+      "description": "XAG/ USD",
+      "proName": "OANDA:XAGUSD"
+    },
+    {
+      "description": "XAG/ EUR",
+      "proName": "OANDA:XAGEUR"
+    }
+  ],
+  "showSymbolLogo": true,
+  "colorTheme": "light",
+  "isTransparent": true,
+  "displayMode": "adaptive",
+  "locale": "en"
+}
+  </script>
 </div>
 <!-- TradingView Widget END -->
+
